65287d6f4a421785019523a868694bb3ff640d32
Roman Suvorov <suvorov83@gmail.com>
2021-12-24 19:06:58 +0300

    app path, snapshots paths, read only snapshots

--- pkg/domain/volume.go
+++ pkg/domain/volume.go
@@ -1,5 +1,14 @@
 package domain
 
+const (
+	Monthly = "monthly"
+	Weekly  = "weekly"
+	Daily   = "daily"
+	Hourly  = "hourly"
+	Boot    = "boot"
+	Manual  = "manual"
+)
+
 // Volume represents btrfs volume.
 type Volume struct {
 	Label  string `json:"label"`
