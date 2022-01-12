26c0cf57ff7f2235d2bb51341ef2fcc31ceb6a99
Roman Suvorov <suvorov83@gmail.com>
2022-01-12 12:52:53 +0300

    move from findMnt.FsOptions to FsRoot

--- pkg/commands/volumes.go
+++ pkg/commands/volumes.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -13,7 +12,7 @@ import (
 
 const (
 	btrfsType      = "btrfs"
-	findmntOptions = "SOURCE,TARGET,FS-OPTIONS,LABEL,UUID"
+	findmntOptions = "SOURCE,TARGET,LABEL,UUID,FSROOT"
 )
 
 type findMntOutput struct {
@@ -21,11 +20,11 @@ type findMntOutput struct {
 }
 
 type findMntVolume struct {
-	Source    string `json:"source"`
-	Target    string `json:"target"`
-	FsOptions string `json:"fs-options"`
-	Label     string `json:"label"`
-	UUID      string `json:"uuid"`
+	Source string `json:"source"`
+	Target string `json:"target"`
+	Label  string `json:"label"`
+	UUID   string `json:"uuid"`
+	FsRoot string `json:"fsroot"`
 }
 
 // GetVolumes returns volumes using findmnt.
@@ -66,27 +65,14 @@ func GetVolumes() ([]domain.Volume, error) {
 	return vv, nil
 }
 
-// return value of string with equal sign
-// like `UUID=some-uuid-here`
-// will return `some-uuid-here`.
-func getValueFromAssignmet(s string) (string, error) {
-	ss := strings.Split(s, "=")
-	if len(ss) != 2 {
-		return "", errors.New("there is no equal sign in the string")
-	}
-
-	v := strings.Trim(ss[1], `"`)
-
-	return v, nil
-}
-
 func buildVolume(fmv findMntVolume) (domain.Volume, error) {
 	v := domain.Volume{
 		Label:      fmv.Label,
-		Device:     strings.Split(fmv.Source, "[")[0],
+		Device:     strings.Split(fmv.Source, "[")[0], // safe even if string without `[` character
 		MountPoint: fmv.Target,
 		UUID:       fmv.UUID,
 		Mounted:    true,
+		Subvol:     getSubvol(fmv.FsRoot),
 	}
 
 	if v.Label == "" {
@@ -100,47 +86,18 @@ func buildVolume(fmv findMntVolume) (domain.Volume, error) {
 		}
 	}
 
-	var err error
-	v.Subvol, err = getSubvolFromOptions(fmv.FsOptions)
-	if err != nil {
-		return domain.Volume{}, err
-	}
-
 	return v, nil
 }
 
-func getSubvolFromOptions(options string) (string, error) {
-	opts := strings.Split(options, ",")
-	if len(opts) == 0 {
-		return "", errors.New("empty options")
+func getSubvol(fsRoot string) string {
+	if fsRoot == "/" {
+		return domain.Subvol5
 	}
 
-	for i := range opts {
-		if strings.HasPrefix(opts[i], "subvol=") {
-			sbvl, err := getValueFromAssignmet(opts[i])
-			if err != nil {
-				return "", err
-			}
-
-			if sbvl == "" {
-				return "", errors.New("subvol is empty")
-			}
-
-			if sbvl == "/" {
-				return domain.Subvol5, nil
-			}
-
-			if strings.HasPrefix(sbvl, "/") { // remove slash from options like `subvol=/@`
-				ss := strings.Split(sbvl, "/")
-				if len(ss) != 2 {
-					return "", fmt.Errorf("subvol is invalid: %s", sbvl)
-				}
-				sbvl = ss[1]
-			}
-
-			return sbvl, nil
-		}
+	if strings.HasPrefix(fsRoot, "/") { // remove slash from fsRoot like `/@`
+		ss := strings.Split(fsRoot, "/")
+		return ss[1]
 	}
 
-	return "", errors.New("there is no subvol in options")
+	return fsRoot
 }
