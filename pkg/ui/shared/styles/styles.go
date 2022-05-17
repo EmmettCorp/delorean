e2709f56fff78b874457459bd604e0b7c5e05144
Roman Suvorov <suvorov83@gmail.com>
2022-05-17 08:22:23 +0300

    fix linter issues

--- pkg/ui/shared/styles/styles.go
+++ pkg/ui/shared/styles/styles.go
@@ -2,10 +2,12 @@ package styles
 
 import "github.com/charmbracelet/lipgloss"
 
-// MainTextStyle is the general application text style.
+// MainStyle is the general application text style.
 // nolint:gochecknoglobals // global on purpose
-var MainDocStyle = lipgloss.NewStyle()
+var (
+	MainDocStyle = lipgloss.NewStyle()
 
-var MainTextStyle = MainDocStyle.
-	Foreground(DefaultTheme.MainText).
-	Bold(true)
+	MainTextStyle = MainDocStyle.
+			Foreground(DefaultTheme.MainText).
+			Bold(true)
+)
